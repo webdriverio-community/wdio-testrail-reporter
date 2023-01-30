8ae5fd67ac5c2c3c5aa0131d2bc7aa2c5b35cc7f
Douglas Turner <dturner@intellicentrics>
2023-01-30 17:28:07 -0600

    Get all case ids from the title. Push all case id results. Updated the error message to be readable.

--- src/reporter.ts
+++ src/reporter.ts
@@ -1,120 +1,167 @@
-import WDIOReporter, { TestStats, SuiteStats } from '@wdio/reporter'
+import WDIOReporter, { TestStats, SuiteStats } from "@wdio/reporter";
 
-import TestRailAPI from './api.js'
-import type { ReporterOptions, TestCase, TestResults } from './types'
+import TestRailAPI from "./api.js";
+import type { ReporterOptions, TestCase, TestResults } from "./types";
 
 export default class TestRailReporter extends WDIOReporter {
-    #api: TestRailAPI
-    #options: ReporterOptions
+    #api: TestRailAPI;
+    #options: ReporterOptions;
 
-    #synced = false
-    #testCases: TestCase[] = []
-    #requestPromises: Promise<unknown>[] = []
+    #synced = false;
+    #testCases: TestCase[] = [];
+    #requestPromises: Promise<unknown>[] = [];
 
-    constructor (options: ReporterOptions) {
-        options = Object.assign(options, { stdout: false })
-        super(options)
+    constructor(options: ReporterOptions) {
+        options = Object.assign(options, { stdout: false });
+        super(options);
 
-        this.#api = new TestRailAPI(options)
-        this.#options = options
+        this.#api = new TestRailAPI(options);
+        this.#options = options;
     }
 
     get isSynchronised() {
-        return this.#synced
+        return this.#synced;
     }
 
-    onTestPass (test: TestStats) {
-        this.#testCases.push({
-            case_id: test.title.split(' ')[0].replace('C', ''),
-            status_id: '1',
-            comment: 'This test case is passed'
-        })
-    }
+    titleToCaseIds(title: string): number[] {
+        let caseIds: number[] = [];
 
-    onTestFail (test: TestStats) {
-        this.#testCases.push({
-            case_id: test.title.split(' ')[0].replace('C', ''),
-            status_id: '5',
-            comment: `This test case is failed:\n ${JSON.stringify(test.errors)}`
-        })
-    }
-
-    onTestSkip (test: TestStats) {
-        this.#testCases.push({
-            case_id: test.title.split(' ')[0].replace('C', ''),
-            status_id: '4',
-            comment: 'This test case is skipped'
-        })
-    }
-
-    onSuiteEnd (suiteStats: SuiteStats) {
-        this.#requestPromises.push(this.#updateSuite(suiteStats))
-    }
-
-    onRunnerEnd () {
-        this.#requestPromises.push(this.#updateTestRun())
-    }
-
-    async sync () {
-        // ensure all request promises were resolved
-        await Promise.all(this.#requestPromises)
-        this.#synced = true
-    }
-
-    #getRunId () {
-        return this.#options.oneReport
-            ? this.#api.getLastTestRun(this.#options.suiteId, this.#options.runName)
-            : this.#api.createTestRun({
-                suite_id: this.#options.suiteId,
-                name: this.#options.runName,
-                include_all: this.#options.includeAll
-            })
-    }
-
-    async #updateSuite (suiteStats: SuiteStats) {
-        const values = {
-            'general': 0,
-            'passed': 0,
-            'failed': 0,
-            'skipped': 0,
-            'errors': [] as string[]
+        let testCaseIdRegExp: RegExp = /\bT?C(\d+)\b/g;
+        let m;
+        while ((m = testCaseIdRegExp.exec(title)) !== null) {
+            let caseId = parseInt(m[1]);
+            caseIds.push(caseId);
         }
+        return caseIds;
+    }
+
+    onTestPass(test: TestStats) {
+        let caseIds = this.titleToCaseIds(test.title);
+        if (caseIds.length > 0) {
+            for (let index = 0; index < caseIds.length; index++) {
+                const element = caseIds[index];
+                // console.log("id test: ", element);
+                this.#testCases.push({
+                    case_id: `${element}`,
+                    status_id: "1",
+                    comment: `${test.fullTitle}`,
+                });
+            }
+        }
+    }
+
+    onTestFail(test: TestStats) {
+        let caseIds = this.titleToCaseIds(test.title);
+        if (caseIds.length > 0) {
+            for (let index = 0; index < caseIds.length; index++) {
+                const element = caseIds[index];
+                // console.log("id test: ", element);
+                const errorMsg = test?.error?.stack?.replace(
+                    /\[\d{1,2}m/gm,
+                    ""
+                );
+                this.#testCases.push({
+                    case_id: `${element}`,
+                    status_id: "5",
+                    comment: `${test.fullTitle}\n\n${errorMsg}`,
+                });
+            }
+        }
+    }
+
+    onTestSkip(test: TestStats) {
+        let caseIds = this.titleToCaseIds(test.title);
+        if (caseIds.length > 0) {
+            for (let index = 0; index < caseIds.length; index++) {
+                const element = caseIds[index];
+                // console.log("id test: ", element);
+                this.#testCases.push({
+                    case_id: `${element}`,
+                    status_id: "4",
+                    comment: `${test.fullTitle} was skipped.`,
+                });
+            }
+        }
+    }
+
+    onSuiteEnd(suiteStats: SuiteStats) {
+        this.#requestPromises.push(this.#updateSuite(suiteStats));
+    }
+
+    onRunnerEnd() {
+        this.#requestPromises.push(this.#updateTestRun());
+    }
+
+    async sync() {
+        // ensure all request promises were resolved
+        await Promise.all(this.#requestPromises);
+        this.#synced = true;
+    }
+
+    #getRunId() {
+        return this.#options.oneReport
+            ? this.#api.getLastTestRun(
+                  this.#options.suiteId,
+                  this.#options.runName
+              )
+            : this.#api.createTestRun({
+                  suite_id: this.#options.suiteId,
+                  name: this.#options.runName,
+                  include_all: this.#options.includeAll,
+              });
+    }
+
+    async #updateSuite(suiteStats: SuiteStats) {
+        const values = {
+            general: 0,
+            passed: 0,
+            failed: 0,
+            skipped: 0,
+            errors: [] as string[],
+        };
 
         for (const test of suiteStats.tests) {
             switch (test.state) {
-            case 'failed': values.failed = values.failed + 1
-                values.failed += 1
-                values.errors.push(`Failed on: ${test.title} \n ${JSON.stringify(test.errors, null, 1)}`)
-                break
-            case 'passed':
-                values.passed += 1
-                break
-            case 'skipped':
-                values.skipped += 1
-                break
+                case "failed":
+                    values.failed = values.failed + 1;
+                    values.failed += 1;
+                    values.errors.push(
+                        `Failed on: ${test.title} \n ${JSON.stringify(
+                            test.errors,
+                            null,
+                            1
+                        )}`
+                    );
+                    break;
+                case "passed":
+                    values.passed += 1;
+                    break;
+                case "skipped":
+                    values.skipped += 1;
+                    break;
             }
         }
 
         if (values.failed > 0) {
-            values.general = 5
+            values.general = 5;
         } else if (values.passed === 0 && values.skipped !== 0) {
-            values.general = 4
+            values.general = 4;
         } else {
-            values.general = 1
+            values.general = 1;
         }
 
         const results: TestResults = {
             status_id: values.general.toString(),
-            comment: JSON.stringify(values, null, 1)
-        }
-        const testId = suiteStats.fullTitle.split(' ')[0].replace('C', '')
-        return this.#api.pushResults(testId, results)
+            comment: JSON.stringify(values, null, 1),
+        };
+        const testId = suiteStats.fullTitle.split(" ")[0].replace("C", "");
+        return this.#api.pushResults(testId, results);
     }
 
-    async #updateTestRun () {
-        const runId = await this.#getRunId()
-        const caseIds = this.#testCases.map((test) => test.case_id)
+    async #updateTestRun() {
+        const runId = await this.#getRunId();
+        const caseIds = this.#testCases.map((test) => test.case_id);
         await this.#api.updateTestRun(runId, caseIds),
-        await this.#api.updateTestRunResults(runId, this.#testCases)
+            await this.#api.updateTestRunResults(runId, this.#testCases);
     }
 }

fa6e561fc0c7dafad302feb122c17e74201be7ee
Spencer LaValle <129799088+spencerlavallesonos@users.noreply.github.com>
2024-01-17 20:32:43 -0700

    Update reporter.ts to allow for multiple CaseIDs (#284)
    * Update reporter.ts to allow for multiple CaseIDs
    
    E.g. 
    
    it(`C00001 C00002 should do two things`)...
    
    * unit tests for PR, WIP
    
    * ts syntax
    
    * Update src/reporter.ts
    
    Co-authored-by: Christian Bromann <git@bromann.dev>
    
    * Adding unit tests
    
    * Update README.md
    
    * updating test.yml
    
    * Updating node version
    
    * Fixing eslint errors, removing unused import, removing dummy test
    
    ---------
    
    Co-authored-by: Luke Greaves <luke.greaves@sonos.com>
    Co-authored-by: Christian Bromann <git@bromann.dev>

--- tests/reporter.test.ts
+++ tests/reporter.test.ts
@@ -1,5 +1,168 @@
-import { test, expect } from 'vitest'
+import { describe, test, expect } from 'vitest'
+import TestRailReporter from '../src/reporter.ts'
+import { ReporterOptions } from '../src/types.ts'
 
-test('I am a test', () => {
-    expect(1).toBe(1)
+import fs from 'fs'
+import path from 'path'
+
+const mockOptions: ReporterOptions = {
+    projectId: '1',
+    suiteId: '1',
+    domain: 'domain.testrail.io',
+    username: 'username',
+    apiToken: 'token',
+    runName: 'RunName',
+    oneReport: true,
+    includeAll: false,
+    caseIdTagPrefix: '',
+    useCucumber: false,
+    logFile: '../logFileToSatisfyReporterOptions.log',
+}
+describe('Single CaseID Extraction', () => {
+    test('Should extract a single CaseID for onTestPass()', () => {
+        const jsonFileName = './fixtures/SinglePassingTest.json'
+        const jsonFilePath = path.join(__dirname, jsonFileName)
+
+        const externalJsonData = JSON.parse(
+            fs.readFileSync(jsonFilePath, 'utf-8')
+        )
+
+        const testStats = {
+            ...externalJsonData,
+            start: new Date(),
+        }
+
+        const testRailReporter = new TestRailReporter(mockOptions)
+        testRailReporter.onTestPass(testStats) // Simulate the onTestPass method
+        expect(testRailReporter.getTestCasesArray().length).toEqual(1)
+        expect(typeof testRailReporter.getTestCasesArray()[0].case_id).toEqual(
+            'string'
+        )
+    })
+    test('Should extract a single CaseID for onTestFail()', () => {
+        const jsonFileName = './fixtures/SingleFailingTest.json'
+        const jsonFilePath = path.join(__dirname, jsonFileName)
+
+        const externalJsonData = JSON.parse(
+            fs.readFileSync(jsonFilePath, 'utf-8')
+        )
+
+        const testStats = {
+            ...externalJsonData,
+            start: new Date(),
+        }
+
+        const testRailReporter = new TestRailReporter(mockOptions)
+        testRailReporter.onTestFail(testStats) // Simulate the onTestPass method
+        expect(testRailReporter.getTestCasesArray().length).toEqual(1)
+        expect(typeof testRailReporter.getTestCasesArray()[0].case_id).toEqual(
+            'string'
+        )
+    })
+    test('Should extract a single CaseID for onTestSkip()', () => {
+        const jsonFileName = './fixtures/SingleSkippedTest.json'
+        const jsonFilePath = path.join(__dirname, jsonFileName)
+
+        const externalJsonData = JSON.parse(
+            fs.readFileSync(jsonFilePath, 'utf-8')
+        )
+
+        const testStats = {
+            ...externalJsonData,
+            start: new Date(),
+        }
+
+        const testRailReporter = new TestRailReporter(mockOptions)
+        testRailReporter.onTestSkip(testStats) // Simulate the onTestPass method
+
+        expect(testRailReporter.getTestCasesArray().length).toEqual(1)
+        expect(typeof testRailReporter.getTestCasesArray()[0].case_id).toEqual(
+            'string'
+        )
+    })
+})
+describe('Multiple CaseID Extraction', () => {
+    test('Should extract multiple CaseIDs for onTestPass()', () => {
+        const jsonFileName = './fixtures/MultiPassingTest.json'
+        const jsonFilePath = path.join(__dirname, jsonFileName)
+
+        const externalJsonData = JSON.parse(
+            fs.readFileSync(jsonFilePath, 'utf-8')
+        )
+
+        const testStats = {
+            ...externalJsonData,
+            start: new Date(),
+        }
+
+        const testRailReporter = new TestRailReporter(mockOptions)
+        testRailReporter.onTestPass(testStats) // Simulate the onTestPass method
+
+        expect(testRailReporter.getTestCasesArray().length).toEqual(3)
+        expect(typeof testRailReporter.getTestCasesArray()[0].case_id).toEqual(
+            'string'
+        )
+    })
+    test('Should extract multiple CaseIDs for onTestFail()', () => {
+        const jsonFileName = './fixtures/MultiFailingTest.json'
+        const jsonFilePath = path.join(__dirname, jsonFileName)
+
+        const externalJsonData = JSON.parse(
+            fs.readFileSync(jsonFilePath, 'utf-8')
+        )
+
+        const testStats = {
+            ...externalJsonData,
+            start: new Date(),
+        }
+
+        const testRailReporter = new TestRailReporter(mockOptions)
+        testRailReporter.onTestFail(testStats) // Simulate the onTestPass method
+
+        expect(testRailReporter.getTestCasesArray().length).toEqual(3)
+        expect(typeof testRailReporter.getTestCasesArray()[0].case_id).toEqual(
+            'string'
+        )
+    })
+    test('Should extract multiple CaseIDs for onTestSkip()', () => {
+        const jsonFileName = './fixtures/MultiSkippedTest.json'
+        const jsonFilePath = path.join(__dirname, jsonFileName)
+
+        const externalJsonData = JSON.parse(
+            fs.readFileSync(jsonFilePath, 'utf-8')
+        )
+
+        const testStats = {
+            ...externalJsonData,
+            start: new Date(),
+        }
+
+        const testRailReporter = new TestRailReporter(mockOptions)
+        testRailReporter.onTestSkip(testStats) // Simulate the onTestPass method
+
+        expect(testRailReporter.getTestCasesArray().length).toEqual(3)
+        expect(typeof testRailReporter.getTestCasesArray()[0].case_id).toEqual(
+            'string'
+        )
+    })
+    test('Should extract five CaseIDs for onTestPass()', () => {
+        const jsonFileName = './fixtures/FiveMultiPassingTest.json'
+        const jsonFilePath = path.join(__dirname, jsonFileName)
+        const externalJsonData = JSON.parse(
+            fs.readFileSync(jsonFilePath, 'utf-8')
+        )
+
+        const testStats = {
+            ...externalJsonData,
+            start: new Date(),
+        }
+
+        const testRailReporter = new TestRailReporter(mockOptions)
+        testRailReporter.onTestPass(testStats) // Simulate the onTestPass method
+
+        expect(testRailReporter.getTestCasesArray().length).toEqual(5)
+        expect(typeof testRailReporter.getTestCasesArray()[0].case_id).toEqual(
+            'string'
+        )
+    })
 })
